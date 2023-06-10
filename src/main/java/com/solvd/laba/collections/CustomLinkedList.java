8905c22560cb80dd6ab10bde98cfeb663e055b35
Luis <luisignaciobeltran5@gmail.com>
2023-06-10 14:32:31 -0700

    re-work on XML file and XSD schema

--- src/main/java/com/solvd/laba/collections/CustomLinkedList.java
+++ src/main/java/com/solvd/laba/collections/CustomLinkedList.java
@@ -3,7 +3,7 @@ package com.solvd.laba.collections;
 /**
  * General Operations of a LinkedList
  * add a node to the end of the list,
- * get data at specific index,
+ * get phone.xsd at specific index,
  * get the size of the list
  * */
 
@@ -15,19 +15,19 @@ public class CustomLinkedList<T> {
 
     // Declaring private static inner class 'Node' w/ generic type parameter T
     private static class Node<T> {
-        T data;         // The data stored the nodes in the linked list
+        T data;         // The phone.xsd stored the nodes in the linked list
         Node<T> next;   // The reference to the next node in the list
 
-        // Defining a constructor for the 'Node' class that takes a parameter 'data'
+        // Defining a constructor for the 'Node' class that takes a parameter 'phone.xsd'
         Node(T data) {
-            this.data = data;   // Initializing the 'data' variable to the value passed in the constructor
+            this.data = data;   // Initializing the 'phone.xsd' variable to the value passed in the constructor
             this.next = null;   // Sets the 'next' variable 'null'
         }
     }
 
-    // Public method that adds a new node with specified data to the end of the list
+    // Public method that adds a new node with specified phone.xsd to the end of the list
     public void add(T data) {
-        Node<T> newNode = new Node<>(data);     // Create a new 'Node' object w/ specified data.
+        Node<T> newNode = new Node<>(data);     // Create a new 'Node' object w/ specified phone.xsd.
         if (head == null) {     // Checks if the 'head' is 'null' indicate that list is empty
             head = newNode;     // If YES 'head' is set to new node.
         } else {
@@ -41,7 +41,7 @@ public class CustomLinkedList<T> {
     }
 
     // Defining a public method 'get' that takes an integer parameter 'index'
-    // method will return the data at the node with specified index in the list.
+    // method will return the phone.xsd at the node with specified index in the list.
     public T get(int index) {
         if (index < 0 || index >= size) {  // check if the given index is out of bounds
             throw new IndexOutOfBoundsException();  // throw an exception if the index is out of bounds
@@ -50,7 +50,7 @@ public class CustomLinkedList<T> {
         for (int i = 0; i < index; i++) {  // loop through the linked list until the current index is reached
             current = current.next;  // set the current node to the next node in the list
         }
-        return current.data;  // return the data stored in the current node
+        return current.data;  // return the phone.xsd stored in the current node
     }
 
     // Defining a public method 'size' that returns the number of nodes in the list
